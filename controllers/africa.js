6148f6d85003302483851fa3cf980610fc710b36
Lucas0356 <lucasmoltedo03@gmail.com>
2023-09-24 16:34:03 -0300

    Se añadió la cant de resultados en el json

--- controllers/africa.js
+++ controllers/africa.js
@@ -10,15 +10,22 @@ const getAfrica = (req = request, res = response) => {
         .then(({ status, data, statusText }) => {
             // handle success
 
+            /* Seleccionamos los datos que vamos a querer mostrar 
+            Nombre del país (en español), bandera, codigo (3 letras), ID (num)
+            nombre de la/s capital/es y los lenguajes hablados en el país */
             const countries = data
             .map(country => ({ name: country.translations.spa.common, 
                 flag: country.flag, code: country.cioc, id: country.ccn3,
                 capital: country.capital, languages: country.languages}));
 
+            // Cantidad de países en el objeto Map
+            const cantCountries = countries.length;
+
             console.log({ status, data, statusText });
             const { results } = data;
             res.status(200).json({
                 status,
+                cant: cantCountries,
                 countriesFromAfrica: countries,
                 statusText,               
             });
