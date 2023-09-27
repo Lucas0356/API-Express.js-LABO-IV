2471c41da56d7f5ecb325335cf51133a026996c5
martinreser <martinreser76@gmail.com>
2023-09-27 18:02:30 -0300

    Añadimos middleware

--- models/server.js
+++ models/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const cors = require('cors'); 
 class Server{
     constructor(){
         this.port = process.env.PORT || 3000;
 
         this.app = express();
 
+        // 1° en Orden
+        this.middleware();
+
+        // 2° en Orden
         this.routers();
     }
 
-    routers(){
+    middleware(){
+        // Habilitar CORS
+        this.app.use(cors());
+
+        // Validar api_key
         this.app.use((req, res, next) => {
             // Obtenemos la api_key pasada como un Query Param.
             const apiKey = req.headers.api_key;
@@ -27,9 +36,21 @@ class Server{
             // Si la api_key es una de las autorizadas, el programa continúa de forma normal.
             next();
         });
+        
+        //Una vez que se configura esta sección publica, NO 
+        //es posible utilizar
+        this.app.use(express.static('public'));
+    }
 
+    routers(){
         this.app.use('/api/v1/countries', require('../routes/america'));
         this.app.use('/api/v1/countries', require('../routes/africa'));
+
+        this.app.all('*', (req, res) => {
+            res.status(404).json({
+                status: 404,
+                message:'Page Not Found'})
+        })
     }
 
     listen(){
